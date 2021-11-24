55fb664bb7ab1019975b9ca56e06bea2f3b2554b
capsshift <myuudisk@gmail.com>
2021-11-24 15:49:47 +0800

    Update ImageProxyService.cs

--- Emby.Plugins.JavScraper/Services/ImageProxyService.cs
+++ Emby.Plugins.JavScraper/Services/ImageProxyService.cs
@@ -80,7 +80,7 @@ namespace Emby.Plugins.JavScraper.Services
             //  /emby/Plugins/JavScraper/Image?url=&type=xx
             if (url.IndexOf("Plugins/JavScraper/Image", StringComparison.OrdinalIgnoreCase) >= 0) //本地的链接
             {
-                var uri = new Uri(url);
+                var uri = new Uri("https://emby.dpfpl.fun:8920"+url);
                 var q = HttpUtility.ParseQueryString(uri.Query);
                 var url2 = q["url"];
                 if (url2.IsWebUrl())
